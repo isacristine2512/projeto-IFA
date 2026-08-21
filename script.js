bb543165c8053dd73c98ba81b2f054620a2aac31
isacristine2512 <cristine.silva.isabelly@escola.pr.gov.br>
2026-08-21 07:40:49 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+let tamanhoFonte = 16;
+
+
+function aumentarFonte(){
+    tamanhoFonte += 2;
+    document.body.style.fontSize = tamanhoFonte + "px";
+}
+
+
+function diminuirFonte(){
+
+
+    if(tamanhoFonte > 10){
+        tamanhoFonte -= 2;
+        document.body.style.fontSize = tamanhoFonte + "px";
+    }
+}
+
+
+function altoContraste(){
+    document.body.classList.toggle("contraste");
+}
+
